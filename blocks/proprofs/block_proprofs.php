011ddb83d7852853e9c874ade1382a28b19a7693
Ty Christensen <tychristensen97@gmail.com>
2020-10-24 11:47:18 -0600

    for safety

--- blocks/proprofs/block_proprofs.php
+++ blocks/proprofs/block_proprofs.php
@@ -6,15 +6,75 @@ class block_proprofs extends block_base {
     // The PHP tag and the curly bracket for the class definition 
     // will only be closed after there is another function added in the next section.
 
+    function CallAPI($method, $url, $data = false)
+    {
+        $curl = curl_init();
+
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // Optional Authentication:
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+
     public function get_content() {
         if ($this->content !== null) {
           return $this->content;
         }
+
+        $url = "https://www.proprofs.com/api/classroom/v1/reports/users/";
+
+        $data = [
+            "token" => "b636adb108cf9ba92eaef5018f5a6d7c",
+            "username" => "legrandconcours@sbcglobal.net",
+            "start" => 1,
+            "num" => 100
+        ];
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Content-Type: application/json'
+        ]);
+
+        $response = json_decode(curl_exec($curl), true);
+        curl_close($curl);
+
+        $studentList = "Name    ID";
+
+        // foreach($response["result"] as $user) {
+        //     $studentList .= "<br>" . $user["Name"] . "    " . $user["ID"];
+        // }
      
-        $this->content         =  new stdClass;
-        $this->content->header = '';
-        $this->content->text   = 'The content of our proprofs block!';
-        $this->content->footer = 'Footer here...';
+        $this->content         =  $response;
+        $this->content->header = 'test response';
+        $this->content->text   = $studentList;
+        $this->content->footer = "";
      
         return $this->content;
     }
--- blocks/proprofs/lang/en/block_proprofs.php
+++ blocks/proprofs/lang/en/block_proprofs.php
@@ -1,5 +1,5 @@
 <?php
-$string['pluginname'] = 'proprofs block';
+$string['pluginname'] = 'ProProfs';
 $string['proprofs'] = 'proprofs';
 $string['proprofs:addinstance'] = 'Add a new proprofs block';
 $string['proprofs:myaddinstance'] = 'Add a new proprofs block to the My Moodle page';
